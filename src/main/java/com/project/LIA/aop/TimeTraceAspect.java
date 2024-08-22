a1bf3b518bf87932330f77df562e3822ac5b2042
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-08-22 11:37:52 +0900

    feat: AOP 시간 측정 메소드

---
+++ src/main/java/com/project/LIA/aop/TimeTraceAspect.java
@@ -0,0 +1,25 @@
+package com.project.LIA.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAspect {
+
+    @Around("execution(* com.project.LIA..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: "+joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long time = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + time + "ms");
+        }
+    }
+
+}
